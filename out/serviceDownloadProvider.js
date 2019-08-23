6d4d089edd749740697696c392f9bb617943e718
ranasaria <ranasaria@outlook.com>
2019-08-22 21:07:46 -0700

    New release 0.1.7 with support for retries

--- out/serviceDownloadProvider.js
+++ out/serviceDownloadProvider.js
@@ -3,6 +3,14 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 'use strict';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require("fs");
 const decompress = require("decompress");
@@ -12,6 +20,8 @@ const tmp = require("tmp");
 const platform_1 = require("./platform");
 const httpClient_1 = require("./httpClient");
 const errors_1 = require("./errors");
+const util_1 = require("util");
+const asyncRetry = require("async-retry");
 /*
 * Service Download Provider class which handles downloading the service client
 */
@@ -66,29 +76,40 @@ class ServiceDownloadProvider {
      * Downloads the service and decompress it in the install folder.
      */
     installService(platform) {
-        const proxy = this._config.proxy;
-        const strictSSL = this._config.strictSSL;
-        return new Promise((resolve, reject) => {
+        return __awaiter(this, void 0, void 0, function* () {
+            const proxy = this._config.proxy;
+            const strictSSL = this._config.strictSSL;
             const fileName = this.getDownloadFileName(platform);
             const installDirectory = this.getInstallDirectory(platform);
             const urlString = this.getGetDownloadUrl(fileName);
-            let pkg = {
+            const pkg = {
                 installPath: installDirectory,
                 url: urlString,
                 tmpFile: undefined
             };
-            this.createTempFile(pkg).then(tmpResult => {
-                pkg.tmpFile = tmpResult;
-                this.httpClient.downloadFile(pkg.url, pkg, proxy, strictSSL).then(_ => {
-                    this.install(pkg).then(result => {
-                        resolve(true);
-                    }).catch(installError => {
-                        reject(installError);
-                    });
-                }).catch(downloadError => {
-                    reject(downloadError);
-                });
+            const existsAsync = util_1.promisify(fs.exists);
+            const unlinkAsync = util_1.promisify(fs.unlink);
+            const downloadAndInstall = () => __awaiter(this, void 0, void 0, function* () {
+                try {
+                    pkg.tmpFile = yield this.createTempFile(pkg);
+                    console.info(`\tdownloading the package: ${pkg.url}`);
+                    console.info(`\t                to file: ${pkg.tmpFile.name}`);
+                    yield this.httpClient.downloadFile(pkg.url, pkg, proxy, strictSSL);
+                    console.info(`\tinstalling the package from file: ${pkg.tmpFile.name}`);
+                    yield this.install(pkg);
+                }
+                finally {
+                    // remove the downloaded package file
+                    if (yield existsAsync(pkg.tmpFile.name)) {
+                        yield unlinkAsync(pkg.tmpFile.name);
+                        console.info(`\tdeleted the package file: ${pkg.tmpFile.name}`);
+                    }
+                }
             });
+            // if this._config.retry is not defined then this.withRetry defaults to number of retries of 0
+            // which is same as without retries.
+            yield withRetry(downloadAndInstall, this._config.retry);
+            return true;
         });
     }
     createTempFile(pkg) {
@@ -109,4 +130,31 @@ class ServiceDownloadProvider {
     }
 }
 exports.ServiceDownloadProvider = ServiceDownloadProvider;
+function withRetry(promiseToExecute, retryOptions = { retries: 0 }) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // wrap function execution with a retry promise
+        // by default, it retries 10 times while backing off exponentially.
+        // retryOptions parameter can be used to configure how many and how often the retries happen.
+        // https://www.npmjs.com/package/promise-retry
+        return yield asyncRetry((bail, attemptNo) => __awaiter(this, void 0, void 0, function* () {
+            try {
+                // run the main operation
+                return yield promiseToExecute();
+            }
+            catch (error) {
+                if (/403/.test(error)) {
+                    // don't retry upon 403
+                    bail(error);
+                    return;
+                }
+                if (attemptNo <= retryOptions.retries) {
+                    console.warn(`[${(new Date()).toLocaleTimeString('en-US', { hour12: false })}] `
+                        + `Retrying...   as attempt:${attemptNo} to run '${promiseToExecute.name}' failed with: '${error}'.`);
+                }
+                // throw back any other error so it can get retried by asyncRetry as appropriate
+                throw error;
+            }
+        }), retryOptions);
+    });
+}
 //# sourceMappingURL=serviceDownloadProvider.js.map
